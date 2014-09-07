dcb4294f7ceb340238a72621247f3283556f6b43
Dan Bikle <dan.bikle@gmail.com>
2014-09-07 15:15:16 -0700

    panera-now

---
+++ matlab_demos/panera1.m
@@ -0,0 +1,10 @@
+% panera1.m
+% I use this script to demo logistic regression of stock market data
+% Get data from yahoo for gspc
+% Create vectors from yahoo data
+% Create in-sample vectors from vectors
+% Create out-of-sample vectors from vectos
+% Predict y-values of oos-vectors
+% Compare predicted y-values to real y-values
+% Compare matlab validation to linux validation
+
