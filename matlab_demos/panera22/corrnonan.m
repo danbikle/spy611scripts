49e879d7492fce89c4836145e4b61b5ced051d1f
Dan Bikle <dan.bikle@gmail.com>
2014-09-20 02:07:01 -0700

    corrnonan-better-now

--- matlab_demos/panera22/corrnonan.m
+++ matlab_demos/panera22/corrnonan.m
@@ -14,12 +14,13 @@ while ( x2 <= length(myx))
   shortx = myx(x1:x2);
   shorty = myy(x1:x2);
   myrcorr(x2) = corr(shortx, shorty);
+  if(isnan(myrcorr(x2)))
+    % If I get NaN, just use the previous val:
+    myrcorr(x2) = myrcorr(x2-1)
+  end
   x1 = x1 + 1;
   x2 = x2 + 1;
 end
-% Assume last value of myrcorr is NaN.
-% The value before is good enough:
-myrcorr(end) = myrcorr(end-1);
 
 % I need a vertical vector:
 myrcorr = myrcorr';
